d1149be129178f532c7e4387b533b3e848c9a65e
FusionPower-Texas <wrg1012@aol.com>
2026-08-20 05:03:12 -0500

    Bust signature script cache again

--- expanded-jsa.js
+++ expanded-jsa.js
@@ -74,7 +74,7 @@
   };
 
   load('app-polish.js?v=20260820');
-  load('expanded-jsa-core.js?v=20260820', () => load('jsa-signatures.js?v=20260820c'));
+  load('expanded-jsa-core.js?v=20260820', () => load('jsa-signatures.js?v=20260820d'));
   load('timekeeping.js?v=20260820', () => {
     load('timekeeping-roster.js?v=20260820b');
     load('timekeeping-report-v2.js?v=20260820');
