80a5e0f115a4e1fcbe00d5896e97fa9e1ccfa536
FusionPower-Texas <wrg1012@aol.com>
2026-08-23 21:14:13 -0500

    Harden CSV exports (expanded-jsa.js)

--- expanded-jsa.js
+++ expanded-jsa.js
@@ -79,9 +79,9 @@
   load('expanded-jsa-core.js?v=20260820', () => {
     load('jsa-signatures.js?v=20260820j', () => load('jsa-signature-layout-fix.js?v=20260820a'));
   });
-  load('timekeeping.js?v=20260823h', () => {
+  load('timekeeping.js?v=20260823i', () => {
     load('timekeeping-roster.js?v=20260823f');
-    load('timekeeping-report-v2.js?v=20260823a', () => {
+    load('timekeeping-report-v2.js?v=20260823b', () => {
       load('timekeeping-polish.js?v=20260820a');
       load('timekeeping-payroll.js?v=20260820a');
     });
