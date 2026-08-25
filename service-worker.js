14166ed44d409e4e56ccde936447a5dd9d032e4e
FusionPower-Texas <wrg1012@aol.com>
2026-08-25 00:47:31 -0500

    Update service-worker.js for sign-out state reset

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'linecrew-pro-shell-v28';
+const CACHE_NAME = 'linecrew-pro-shell-v29';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -6,7 +6,7 @@ const APP_SHELL = [
   '/icons/linecrew-pro-180.png',
   '/icons/linecrew-pro-192.png',
   '/icons/linecrew-pro-512.png',
-  '/expanded-jsa.js?v=20260823i'
+  '/expanded-jsa.js?v=20260825a'
 ];
 
 self.addEventListener('install', (event) => {
